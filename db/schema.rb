223dae12a3cb1ebe68ce8d2c98b181c1a84eab34
Eugene Domotenko <e.domotenko@gmail.com>
2016-11-24 01:24:56 +0200

    add Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123230337) do
+ActiveRecord::Schema.define(version: 20161123231938) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
