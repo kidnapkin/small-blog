d2e17a3122e27597180c6daa98d436e045956b22
Eugene Domotenko <e.domotenko@gmail.com>
2016-11-24 01:18:15 +0200

    basic authentication/authorization

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  get 'sessions/new'
+
+  root      'pages#home'
+  get       '/about',   to: 'pages#about'
+  get       '/contact', to: 'pages#contact'
+  post      '/signup',  to: 'users#create'
+  get       '/signup',  to: 'users#new'
+  get       '/login',   to: 'sessions#new'
+  post      '/login',   to: 'sessions#create'
+  delete    '/logout',  to: 'sessions#destroy'
+
+  resources :users
+end
