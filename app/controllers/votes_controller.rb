81335be499238221c9e7c02256cfcbd33d865428
Eugene <e.domotenko@gmail.com>
2016-12-08 09:53:24 +0000

    enforce votes security :metal:

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,7 @@
 class VotesController < ApplicationController
   before_action :set_comment, except: [:destroy]
-
+  before_action :correct_user, only: [:create]
+  
   def create
     @vote = Vote.create(vote_params)
     if @vote.save
@@ -29,5 +30,14 @@ class VotesController < ApplicationController
 
   def set_comment
     @comment = Comment.find(params[:comment_id])
+  end
+  
+  def correct_user
+    @user = User.find(params[:user_id])
+    unless @user == current_user
+      respond_to do |format|
+        format.js { render text: "alert('Not allowed');" }
+      end
+    end
   end
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,7 @@
 module CommentsHelper
   def nested_comments(comments)
     safe_join(comments.map do |comment, sub_comment|
-      render(comment) + content_tag(:div, nested_comments(sub_comment), class: 'nested_comments')
+      render(comment).html_safe + content_tag(:div, nested_comments(sub_comment).html_safe, class: 'nested_comments')
     end)
   end
 end
